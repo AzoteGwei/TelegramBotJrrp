7a45c8c57a6a1d0d4450b33408b897912ee9098f
Sen Yue <sencat31@outlook.com>
2023-08-30 01:56:14 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# TelegramBotJrrp
+
+这个东西只有一个作用 ———— 今日人品。
+
+## 依赖
+
+```bash
+pip install pyTelegramBotAPI
+```
+
+## 运行
+
+```bash
+python3 main.py yourtoken
+```
---
+++ main.py
@@ -0,0 +1,60 @@
+# 使用 python3 main.py yourtoken 来启动，比如 python3 main.py 114514:ABCDEFGO_HIJKLMNOxE
+import telebot
+import random
+from datetime import datetime, timedelta
+import sys
+
+
+bot = telebot.TeleBot(str(sys.argv[1]), parse_mode="MARKDOWN")
+
+
+@bot.message_handler(commands=["jrrp"])
+def send_jrrp(message):
+    bot.reply_to(message, jrrp_text_init(from_input_get_score(message.from_user.id)))
+
+
+def jrrp_text_init(nub_in):
+    nub = int(nub_in)
+    if nub == 100:
+        return "今天的人品是：" + str(nub_in) + "\n" + "100 人品好评!!!"
+    elif nub >= 90:
+        return "今天的人品是：" + str(nub_in) + "\n" + "今天的人品非常不错呢"
+    elif nub >= 70:
+        return "今天的人品是：" + str(nub_in) + "\n" + "哇,人品还挺好的!"
+    elif nub >= 60:
+        return "今天的人品是：" + str(nub_in) + "\n" + "今天是 非常¿ 不错的一天呢!"
+    elif nub > 50:
+        return "今天的人品是：" + str(nub_in) + "\n" + f"{nub} 你的人品还不错呢"
+    elif nub == 50:
+        return "今天的人品是：" + str(nub_in) + "\n" + "五五开！"
+    elif nub >= 40:
+        return "今天的人品是：" + str(nub_in) + "\n" + f"还好还好只有 {nub}"
+    elif nub >= 20:
+        return "今天的人品是：" + str(nub_in) + "\n" + f"{nub} 这数字太....要命了"
+    elif nub >= 0:
+        return "今天的人品是：" + str(nub_in) + "\n" + "抽大奖¿"
+
+
+def from_input_get_score(user_id):
+    random.seed(int(when_is_now_in_utc_plus_8()) + int(user_id))
+    score = random.randint(0, 100)
+
+    return score
+
+
+def when_is_now_in_utc_plus_8():
+    # 获取当前时间
+    current_time = datetime.utcnow()
+
+    # 将当前时间调整为东八区时间
+    eastern_eight_time = current_time + timedelta(hours=8)
+
+    # 提取年月日
+    year = eastern_eight_time.year
+    month = eastern_eight_time.month
+    day = eastern_eight_time.day
+
+    return int(str(year) + str(month) + str(day))
+
+
+bot.infinity_polling()
